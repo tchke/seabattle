f25a7182f4500e64775f4873e2e39d2f863d1b68
tchke <tchk953@gmail.com>
2018-06-24 17:29:57 +0300

    Connected boards to state

---
+++ src/OwnBoard.js
@@ -0,0 +1,10 @@
+import { connect } from 'react-redux';
+import Board from "./Board";
+
+const mapStateToProps = (state) => {
+    return {
+        state
+    };
+}
+
+export default connect(mapStateToProps)(Board);
