5fef73a19d002f0d4916f37803f20905904ea591
tchke <tchk953@gmail.com>
2018-07-13 16:12:28 +0300

    unit tests

--- src/__test__/Square.spec.js
+++ src/__test__/Square.spec.js
@@ -1,3 +1,32 @@
-test('Square markup', () => {
+import React from 'react';
+import Square from '../Square';
+import renderer from 'react-test-renderer';
 
+it('empty square should be rendered correctly', () => {
+    const tree = renderer
+        .create(<Square></Square>)
+        .toJSON();
+    expect(tree).toMatchSnapshot();
 });
+
+it('open empty square should be rendered correctly', () => {
+    const tree = renderer
+        .create(<Square open={true}></Square>)
+        .toJSON();
+    expect(tree).toMatchSnapshot();
+});
+
+it('occupied square should be rendered correctly', () => {
+    const tree = renderer
+        .create(<Square has={true}></Square>)
+        .toJSON();
+    expect(tree).toMatchSnapshot();
+});
+
+it('open occupied square should be rendered correctly', () => {
+    const tree = renderer
+        .create(<Square has={true} open={true}></Square>)
+        .toJSON();
+    expect(tree).toMatchSnapshot();
+});
+
--- src/__test__/sum.spec.js
+++
@@ -1,5 +0,0 @@
-import { sum } from '../sum.js';
-
-test('first test', () => {
-    expect(sum(2,3)).toBe(5);
-});
